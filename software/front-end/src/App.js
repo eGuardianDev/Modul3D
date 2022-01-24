6399a0c23e4320a8587761f42ffca1e086532da0
eGuardianDev <cvetomir200419@abv.bg>
2022-01-24 11:26:44 +0200

    Front-End-Added

--- software/front-end/src/App.js
+++ software/front-end/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navigation from "./Elements/Navigation"
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation  />
+      <Switch>
+        
+        <Route path="/" exact component={Home}/>
+        <Route path="/whattheprintersees" component={Home}/>
+      </Switch>
     </div>
+      </BrowserRouter>
   );
+}
+//p 190 text stone превод
+//p 191 
+//p 46/47 Oral communicaiton translated (ex 4 too)
+const Home = () =>{
+  return <h1>Doge</h1>
 }
 
 export default App;
---
+++ software/front-end/src/Elements/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Navigation extends React.Component{
+render(){
+ 
+  return(
+    <div className="NavigationClass">
+    <button>Home</button>
+    <button>Home</button>
+    
+    </div>
+  );
+}
+}
+
+export default Navigation;
