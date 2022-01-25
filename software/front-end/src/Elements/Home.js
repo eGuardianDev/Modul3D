eb5d9133dd0894e79ebf03b614bf5f265ec18767
eGuardianDev <cvetomir200419@abv.bg>
2022-01-25 22:49:41 +0200

    Update

--- software/front-end/src/Elements/Home.js
+++ software/front-end/src/Elements/Home.js
@@ -1,25 +1,28 @@
+import { isFrozen } from "core-js/core/object";
+import React from "react";
 
-async function Test(){
-    return(await fetch('http://192.168.0.106:5000')
-    .then(response=>response.json()));
-    
-
-}
-
-const Home = () =>{
-    let data = Test();
-    console.log(data)
-    return(
+class Home extends React.Component {
+    constructor(){
+        super();
+        this.state = {backend: 0, printing: 0,printingState: 0};
+    }
+    async componentDidMount(){
+         await fetch('http://192.168.0.106:5000')
+        .then(response=>{response.json()})
+        .then(data => this.setState({backend:true,printing: data.printing, printingState: data.printingState}));    
+    }
+    render(){
+        return(
     <div className='DisplayedPage'>
       <div>
         <h1>Main data</h1>
-        <h1>Status: {}</h1>
-        <h1>Printing: offline</h1>
+        <h1>Status: {this.state.backend ? "online" : "offline"}</h1>
+        <h1>Printing: {this.state.printing ? "online" : "offline"}</h1>
         <h1>Printing state: 0%</h1>
-        <h1>Back end status: offline</h1>
       </div>
     </div>
     )
+    }
 }
 
 export default Home;
